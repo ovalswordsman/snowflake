273c557f6243976eb4882e6f18566fb7c3ce7393
Kushagra Singh <54627996+ovalswordsman@users.noreply.github.com>
2023-04-28 13:57:37 +0530

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -11,20 +11,22 @@ GRANT ROLE PII TO ROLE ACCOUNTADMIN;
 
 -- 2
 USE ROLE ACCOUNTADMIN;
+
 CREATE WAREHOUSE IF NOT EXISTS assignment_wh
 WAREHOUSE_SIZE = 'MEDIUM'
 WAREHOUSE_TYPE = 'STANDARD'
 AUTO_SUSPEND = 60
 AUTO_RESUME = TRUE;
 
+-- Grant permissions to create a database and access the warehouse to the Admin role
+GRANT CREATE DATABASE ON account TO ROLE Admin;
+GRANT ALL PRIVILEGES ON WAREHOUSE assignment_wh TO ROLE Admin;
 
 -- 3
+
 USE ROLE ADMIN;
 
 -- 4
-Use role accountadmin;
-GRANT CREATE DATABASE ON ACCOUNT TO ROLE ADMIN;
-use role admin;
 
 CREATE DATABASE IF NOT EXISTS assignment_db;
 
@@ -38,13 +40,13 @@ CREATE OR REPLACE TABLE my_schema.EMPLOYEE (
     LAST_NAME VARCHAR(255),
     EMAIL VARCHAR(255),
     PHONE_NUMBER VARCHAR(255),
-    salary number,
+    salary string,
     department_id number,
     etl_ts timestamp default current_timestamp(),
     -- for getting the time at which the record is getting inserted
-    etl_by varchar default 'snowsight',
+    etl_by string default 'snowsight',
     -- for getting application name from which the record was inserted
-    file_name varchar -- for getting the name of the file used to insert data into the table.
+    file_name string -- for getting the name of the file used to insert data into the table.
 );
 
 -- 7
@@ -53,66 +55,30 @@ CREATE OR REPLACE FILE FORMAT my_json_format TYPE = JSON;
 CREATE TABLE variant_table(variant_data variant);
 
 
+
 -- 8
--- External stages
+-- External stage
 CREATE or replace STAGE external_stage
 url = 's3://snowflake-assgn';
 list @external_stage;
 
 -- Internal stages
-
 CREATE OR REPLACE STAGE internal_stage
-  file_format = (type = 'CSV' FIELD_DELIMITER = '|' SKIP_HEADER = 1);
+  file_format = (type = 'CSV' FIELD_DELIMITER = ';' SKIP_HEADER = 1);
   
 -- Using snowsql run the following command
 put file:///Users/kush/Desktop/employees.csv @internal_stage;
 list @internal_stage;
 
-CREATE OR REPLACE TABLE employee_internal_stage (
-        Employee_ID NUMBER,
-        FIRST_NAME VARCHAR(255),
-        LAST_NAME VARCHAR(255),
-        EMAIL VARCHAR(255),
-        PHONE_NUMBER VARCHAR(255),
-        salary number,
-        department_id number,
-        etl_ts timestamp default current_timestamp(),
-        -- for getting the time at which the record is getting inserted
-        etl_by varchar default 'snowsight',
-        -- for getting application name from which the record was inserted
-        file_name varchar -- for getting the name of the file used to insert data into the table.
-    );
-
-    CREATE OR REPLACE TABLE employee_external_stage (
-        Employee_ID NUMBER,
-        FIRST_NAME VARCHAR(255),
-        LAST_NAME VARCHAR(255),
-        EMAIL VARCHAR(255),
-        PHONE_NUMBER VARCHAR(255),
-        salary number,
-        department_id number,
-        etl_ts timestamp default current_timestamp(),
-        -- for getting the time at which the record is getting inserted
-        etl_by varchar default 'snowsight',
-        -- for getting application name from which the record was inserted
-        file_name varchar -- for getting the name of the file used to insert data into the table.
-    );
-
-    
--- Copying from external stage into employee_external_stage
-CREATE OR REPLACE FILE FORMAT my_csv_format TYPE = 'CSV' FIELD_DELIMITER = ',' SKIP_HEADER = 1;
-use warehouse compute_wh;
 
 
-COPY INTO employee_internal_stage(
-    Employee_ID,
-    FIRST_NAME,
-    LAST_NAME,
-    EMAIL,
-    PHONE_NUMBER,
-    salary,
-    department_id,
-    file_name
+-- 9
+-- Copying from external stage into employee
+CREATE OR REPLACE FILE FORMAT my_csv_format TYPE = 'csv' FIELD_DELIMITER = ';' SKIP_HEADER = 1;
+
+
+
+COPY INTO employee(Employee_ID,FIRST_NAME,LAST_NAME,EMAIL,PHONE_NUMBER,salary,department_id,file_name
     )
 FROM
     (
@@ -126,37 +92,32 @@ FROM
             emp.$7,
             METADATA$FILENAME
         FROM
-            @internal_stage/customer.csv.gz (file_format => my_csv_format) emp
+            @internal_stage/employees.csv.gz (file_format => my_csv_format) emp
     );  
 
+SELECT * FROM employee;
+truncate employee;
+COPY INTO variant_table FROM (
+SELECT PARSE_JSON('{
+    "Employee_ID": "' || $1 || '",
+    "first_name": "' || $2 || '",
+    "last_name": "' || $3 || '",
+    "email": "' || $4 || '",
+    "phone_number": "' || $5 || '",
+    "salary" : "' || $6 || '",
+    "department_id" : "' || $7 || '",
+    "elt_ts" : "' || CURRENT_TIMESTAMP() || '",
+    "elt_by" : "snowsight",
+    "file_name" : "' || METADATA$FILENAME || '"
+  }')
+  FROM @external_stage/employees.csv
+  (file_format => my_csv_format) emp
+  );
 
-COPY INTO employee_external_stage(
-    Employee_ID,
-    FIRST_NAME,
-    LAST_NAME,
-    EMAIL,
-    PHONE_NUMBER,
-    salary,
-    department_id,
-    file_name
-    )
-FROM
-    (
-        SELECT
-            emp.$1,
-            emp.$2,
-            emp.$3,
-            emp.$4,
-            emp.$5,
-            emp.$6,
-            emp.$7,
-            METADATA$FILENAME
-        FROM
-            @external_stage/customer.csv (file_format => my_csv_format) emp
-    );  
-    
-    
-  
+  select * from variant_table;
+
+select ($1) from  @internal_stage/employees.csv.gz(file_format => my_csv_format) ;
+
 -- 10
 -- First, create a stage 
 CREATE FILE FORMAT my_parquet_format TYPE = parquet;
@@ -176,12 +137,15 @@ FROM
             FILE_FORMAT => 'my_parquet_format'
         )
     );
-    
- -- 11
-SELECT * from @my_parquet_stage/userdata1.parquet;  
+
+-- 11
+SELECT $1:birthdate::String as bday from @my_parquet_stage;   
+
+
 
 -- Question - 12
     -- Add masking policy to the PII columns such that fields like email, phone number, etc. show as **masked** to a user with the developer role. If the role is PII the value of these columns should be visible
+USE ROle ACCOUNTADMIN;
 CREATE OR REPLACE MASKING POLICY email_mask AS (VAL string) RETURNS string -> CASE
     WHEN CURRENT_ROLE() = 'PII' THEN VAL
     ELSE '****MASK****'
@@ -200,12 +164,12 @@ ALTER TABLE IF EXISTS employee MODIFY phone_number SET MASKING POLICY contact_ma
 SELECT * FROM employee;
 
 -- Displaying data from PII view
-USE ROLE ACCOUNTADMIN;
+
 -- Granting required previlages to role PII
 GRANT ALL PRIVILEGES ON WAREHOUSE assignment_wh TO ROLE PII;
 GRANT USAGE ON DATABASE ASSIGNMENT_DB TO ROLE PII;
 GRANT USAGE ON SCHEMA ASSIGNMENT_DB.MY_SCHEMA TO ROLE PII;
-GRANT SELECT ON TABLE assignment_db.my_schema.employeeTO ROLE PII;
+GRANT SELECT ON TABLE assignment_db.my_schema.employee TO ROLE PII;
 
 USE ROLE PII;
 SELECT * FROM employee LIMIT 10;
